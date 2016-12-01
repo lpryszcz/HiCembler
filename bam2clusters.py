9485aeb390820304d5d3b46013c79a97eca17b02
lpryszcz <l.p.pryszcz@gmail.com>
2016-12-01 12:16:26 +0100

    v1.01f pickle

--- bam2clusters.py
+++ bam2clusters.py
@@ -386,8 +386,9 @@ def cluster_contigs(outbase, d, bin_chr, bin_position, threads=4, frac=0.8,
     
     dZ = []
     n = d.shape[0]
-    if n < maxnumchr:
-        maxnumchr = n
+    subset = int(round(n * frac))
+    if subset < maxnumchr:
+        maxnumchr = subset
     ''' 24 vs 31s on 4 cores - it's not worth as need to pickle large matrix and pass it through
     args = ((d, prng, n, frac) for i in range(iterations))
     p = Pool(threads)
@@ -397,7 +398,6 @@ def cluster_contigs(outbase, d, bin_chr, bin_position, threads=4, frac=0.8,
     '''
     for i in range(iterations):
         sys.stderr.write(' %s / %s  \r'%(i+1, iterations))
-        subset = int(round(n * frac))
         selection = prng.permutation(n)[:subset]
         iZ = fastcluster.linkage(d[selection, :][:, selection][np.triu_indices(subset, 1)], method="ward")
         dZ.append(np.diff(iZ[:, 2]))
@@ -406,7 +406,6 @@ def cluster_contigs(outbase, d, bin_chr, bin_position, threads=4, frac=0.8,
     mean_step_len = np.mean(np.array(dZ), 0)
     nchr = np.argmax(mean_step_len[::-1][:maxnumchr]) + 2
     logger("  number of chromosomes: %s"%nchr);
-    print n, len(mean_step_len), maxnumchr, len(np.arange(maxnumchr, 1, -1)), len(mean_step_len[-maxnumchr+1:])
     
     plt.figure(figsize = (15, 5))
     plt.title("Number of clusters estimation")
