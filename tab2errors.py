f4854b912e9e73c06dfc1ae6bd4f1dea8ed7f227
Leszek <lpryszcz@MacBook-Pro>
2016-12-09 12:22:50 +0200

    tab2errors.py full stats; missing intra-chr transl end recognition

--- tab2errors.py
+++ tab2errors.py
@@ -6,6 +6,7 @@ import os, sys, gzip
 from datetime import datetime
 #from FastaIndex import FastaIndex
 from collections import Counter
+import numpy as np
 	
 def generate_tab(ref, fasta, threads=4):
 	"""Generate .tab.gz its fname"""
@@ -35,37 +36,95 @@ def get_best_chr(matches):
 	beststrand, salg = strandc.most_common(1)[0]#; print strandc
 	return bestt, talg, beststrand, total_alg
 
+events = ["correct", "inversion", "intra-chr translocation", "inter-chr translocation"]
 def get_blocks(matches, bestt, beststrand):
 	"""Parse matches and report synteny blocks"""
 	qstart, qend, t, tstart, tend, strand = matches[0]
 	blocks = [[(t, tstart, tend-tstart, strand),]]#matches[0][2:6],]]
+	breaks = [events[0]]
 	event = False
 	reverse = False
 	for i in xrange(1, len(matches)):
 		qstart, qend, t, tstart, tend, strand = matches[i]#; print matches[i]
-		# inversion or inter-chromosomal translocation 
-		# or intra-chromosomal translocation: tstart < previos for + or tstart > previous for -
-		if strand != matches[i-1][-1] or t != matches[i-1][2] \
-		  or strand == "+" and tstart < matches[i-1][3]\
-		  or strand == "-" and tstart > matches[i-1][3]:
+		# 1 inversion 
+		if strand != matches[i-1][-1]:
 			blocks.append([])
+			if strand!=beststrand: 
+				breaks.append(events[1])
+			else:
+				breaks.append(events[0])
+		# 2 or intra-chromosomal translocation: tstart < previos for + or tstart > previous for -
+		elif strand == "+" and tstart < matches[i-1][3] or strand == "-" and tstart > matches[i-1][3]:
+			blocks.append([])
+			breaks.append(events[2])
+			# need to get end of intra-translocation! but how?
+		# 3 inter-chromosomal translocation
+		elif t != matches[i-1][2]:
+			blocks.append([])
+			if t!=bestt:
+				breaks.append(events[3])
+			else:
+				breaks.append(events[0])
 		elif blocks[-1]:
 			# get gap size difference
 			estgap = abs(qstart-matches[-1][1])
 			trugap = abs(tstart-matches[-1][4])
 		# add match
 		blocks[-1].append((t, tstart, tend-tstart, strand))#matches[i][2:6])
-	return blocks
+	return blocks, breaks
+
+def process_hits(ref, fasta):
+	"""Report rearrengements from assembly"""
+	q2matches, queries, targets = get_matches(ref, fasta)
+    # process queries by descending size
+	tblocks = []
+	header = "# query\tbest target\taligned\tbest target size\t[%]\tquery aligned\tquery size\t[%]\t# blocks"
+	for e in events: 
+		header += "\t# %s\t%s [bp]\t[%s]" % (e, e, '%')
+	print header
+	totdata = np.zeros(header.count('\t')-1)
+	for i, q in enumerate(sorted(queries, key=lambda x: queries[x], reverse=True), 1): # 
+	    # get best chromosome match
+		bestt, talg, beststrand, total_alg = get_best_chr(q2matches[q])
+		# get synteny blocks and event types
+		blocks, breaks = get_blocks(q2matches[q], bestt, beststrand)
+		# get no. of each event and its size
+		tblocks += blocks
+		types = {e: 0 for e in events} #Counter(breaks)
+		typesizes = {e: 0 for e in events}
+		for i, (block, t) in enumerate(zip(blocks, breaks), 1): 
+			types[t] += 1
+			typesizes[t] += sum(b[2] for b in block)
+		'''
+		if bestt != 'CP002688': continue 
+		for i, (block, t) in enumerate(zip(blocks, breaks), 1): 
+			print i, len(block), block[:2], block[-2:]
+		print types, sum(types.values())
+		print talg, sum(typesizes.values()), typesizes
+		return 
+		#'''
+		# report
+		data = [q, bestt, talg, targets[bestt], 100.*talg/targets[bestt]]
+		data += [total_alg, queries[q], 100.*total_alg/queries[q], len(blocks)]
+		for e in events:
+			data += [types[e], typesizes[e], 100.*typesizes[e]/sum(typesizes.values())]
+		print "\t".join(map(str, data))
+		totdata += data[2:]
+	# recaulculate %
+	totdata[2] = 100.*totdata[0]/totdata[1]
+	totdata[5] = 100.*totdata[3]/totdata[4]
+	indices = [8,11,14,17]#; print sum(totdata[indices])
+	for i in indices: 
+		totdata[i+1] = 100.*totdata[i]/sum(totdata[indices])
+	print "SUM\t-\t"+"\t".join(map(str, totdata))
 
 def get_name(name): return name.split()[0].split('|')[-1].split('.')[0]
-	
-def process_hits(ref, fasta, identityTh=0.9, overlapTh=.66, minscore=100, threads=4):
+
+def get_matches(ref, fasta, identityTh=0.9, overlapTh=.66, minscore=100, threads=4):
     """Generate & process LASTal hits"""
     # execute last and get best query-to-reference matches only
     tabfn = generate_tab(ref, fasta, threads)
-    q2matches = {}
-    queries = {}
-    targets = {}
+    q2matches, queries, targets = {}, {}, {}
     for l in gzip.open(tabfn): 
         if l.startswith('#'):
             continue
@@ -90,22 +149,7 @@ def process_hits(ref, fasta, identityTh=0.9, overlapTh=.66, minscore=100, thread
         	queries[q] = qsize
         # store
         q2matches[q].append((qstart, qend, t, tstart, tend, qstrand))
-        
-    # process queries by descending size
-    tblocks = []
-    print "# query\tbest target\taligned\tbest target size\t[%]\tquery aligned\tquery size\t[%]\tno of blocks"
-    for i, q in enumerate(sorted(queries, key=lambda x: queries[x], reverse=True), 1): # 
-	    # get best chromosome match
-		bestt, talg, beststrand, total_alg = get_best_chr(q2matches[q])
-		if bestt != 'CP002688': continue 
-		blocks = get_blocks(q2matches[q], bestt, beststrand)
-		print q, bestt, talg, targets[bestt], 100.*talg/targets[bestt], total_alg, queries[q], 100.*total_alg/queries[q], len(blocks)
-		tblocks += blocks
-		#'''
-		for i, b in enumerate(blocks, 1): 
-			print i, len(b), b[:2], b[-2:]
-		return 
-		#'''
+    return q2matches, queries, targets
     print "%s blocks for %s contigs"%(len(tblocks), len(queries))
 
 def tab2errors():
